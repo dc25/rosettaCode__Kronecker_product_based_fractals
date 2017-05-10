829fe8dcf60ea76bf523652d7a5a6ec283401423
Dave Compton <davecompton7@gmail.com>
2017-05-10 16:26:53 +0000

    added comments

--- Main.hs
+++ Main.hs
@@ -5,11 +5,15 @@ import Data.Map as DM (Map, fromList)
 import Data.Text (Text, pack)
 import Data.List (transpose)
 
--- show the Vicsek and Sierpinsky fractals
+-- show the Vicsek and Sierpinski fractals
 main :: IO ()
 main = mainWidget $ do 
+  elAttr "h1" ("style" =: "color:black") $ text "Kroneker Product Based Fractals" 
+  el "br" $ return ()
+  elAttr "h2" ("style" =: "color:brown") $ text "Vicsek Fractal" 
   showFractal [[0, 1, 0] ,[1, 1, 1] ,[0, 1, 0] ]
   el "br" $ return ()
+  elAttr "h2" ("style" =: "color:brown") $ text "Sierpinski Fractal" 
   showFractal [[1, 1, 1] ,[1, 0, 1] ,[1, 1, 1] ]
 
 -- size in pixels of an individual cell
@@ -68,6 +72,7 @@ showCell x (y,on) =
      else
        return ()
 
+-- Wrapper around elDynAttrNS'
 elSvgns :: MonadWidget t m => Text -> Dynamic t (Map Text Text) -> m a -> m a
 elSvgns t m ma = do
     (el, val) <- elDynAttrNS' (Just "http://www.w3.org/2000/svg") t m ma
